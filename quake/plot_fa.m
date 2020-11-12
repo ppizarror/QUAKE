54dac4bcbdbcd49cd205fdb0f42afc91bb78777a
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-11-12 00:36:06 -0300

    Elimina famin y famax

--- quake/plot_fa.m
+++ quake/plot_fa.m
@@ -1,19 +1,18 @@
-function [w, faw] = plot_fa(fa, wmin, wmax, famin, famax, plot_title, yaxis_label, logx, NPOINTS, plot_colorw, show_legend)
-%PLOT_FA Grafica la funcion del factor de amplificacion.
+function [w, faw] = plot_fa(fa, wmin, wmax, plot_title, yaxis_label, logx, NPOINTS, plot_colorw, show_legend)
+% PLOT_FA: Grafica la funcion del factor de amplificacion.
 %
-%   plot = plot_fa(fa, wmin, wmax, famin, famax, 'Plot title');
-%   plot = plot_fa(fa, wmin, wmax, famin, famax, 'Plot title');
-%   plot = plot_fa(fa, wmin, wmax, famin, famax, 'Plot title', false);
-%   plot = plot_fa(fa, wmin, wmax, famin, famax, 'Plot title', true, 'FA');
-%   plot = plot_fa(fa, wmin, wmax, famin, famax, 'Plot title', true, 'FA', 1000);
-%   plot = plot_fa(fa, wmin, wmax, famin, famax, 'Plot title', true, 'FA', 100, 'k');
-%   plot = plot_fa(fa, wmin, wmax, famin, famax, 'Plot title', true, 'FA', 100, 'k, false);
+%   plot = plot_fa(fa, wmin, wmax, 'Plot title');
+%   plot = plot_fa(fa, wmin, wmax, 'Plot title');
+%   plot = plot_fa(fa, wmin, wmax, 'Plot title', false);
+%   plot = plot_fa(fa, wmin, wmax, 'Plot title', true, 'FA');
+%   plot = plot_fa(fa, wmin, wmax, 'Plot title', true, 'FA', 1000);
+%   plot = plot_fa(fa, wmin, wmax, 'Plot title', true, 'FA', 100, 'k');
+%   plot = plot_fa(fa, wmin, wmax, 'Plot title', true, 'FA', 100, 'k, false);
 %
 % Parametros:
 %   fa              Funcion factor de amplificacion
 %   wmin            Frecuencia minima de evaluacion, eje x
 %   wmax            Frecuencia maxima de evaluacion, eje x
-%   famax           Valor maximo de FA en el eje y
 %   plot_title      Titulo del grafico
 %   yaxis_label     Label de la leyenda en y
 %   logx            Plot en semilogx
@@ -23,7 +22,7 @@ function [w, faw] = plot_fa(fa, wmin, wmax, famin, famax, plot_title, yaxis_labe
 
 %% Inicia variables
 if ~exist('NPOINTS', 'var')
-    NPOINTS = 100;
+    NPOINTS = 1000;
 end
 if ~exist('plot_colorw', 'var')
     plot_colorw = 'k';
@@ -46,7 +45,10 @@ movegui(plt, 'center');
 set(gcf, 'name', plot_title);
 
 %% Crea las variables
-w = linspace(0.01, wmax, NPOINTS);
+if wmax <= wmin
+    error('wmin debe ser menor que wmax');
+end
+w = linspace(wmin, wmax, NPOINTS);
 faw = zeros(NPOINTS, 1);
 for i = 1:NPOINTS
     faw(i) = fa(w(i));
@@ -54,17 +56,15 @@ end
 
 %% Grafica
 if ~logx
-    plot(w, faw, plot_colorw);
+    plot(w, abs(faw), plot_colorw);
 else
-    semilogx(w, faw, plot_colorw);
+    semilogx(w, abs(faw), plot_colorw);
 end
 hold on;
 grid on;
 title(plot_title);
 xlabel('$\omega$', 'interpreter', 'latex');
 ylabel(yaxis_label, 'interpreter', 'latex');
-ylim([famin, famax]);
-xlim([wmin, wmax]);
 if show_legend
     legend({yaxis_label}, 'Location', 'northwest')
 end
