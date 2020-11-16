75ff44749cebf0e4634e0d8a3a569dbe9b830696
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-11-15 22:05:12 -0300

    Cambia FT por FA

--- quake/quake_velt.m
+++ quake/quake_velt.m
@@ -18,6 +18,27 @@ function quake_velt(rho, Vs, D, H, E1, T, dh, dt, plot_normalize, plot_pause, pl
 %   mult_umax       Factor en que crece umax
 %   disp_legend     Muestra la leyenda
 %   show_max        Muestra el maximo de u(z,t) por cada z
+%
+% MIT License
+% Copyright (c) 2019-2020 Pablo Pizarro R.
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
 
 %% Inicia variables
 if ~exist('plot_normalize', 'var')
@@ -158,7 +179,7 @@ for i = 1:length(t)
     ylim([0, zz]);
     xlabel(plot_xlabel, 'interpreter', 'latex');
     ylabel(plot_ylabel, 'interpreter', 'latex');
-    title(sprintf('Quake Visco-Elastico | Re(u):azul, Im(u):rojo | FT: %.3f', ft), 'interpreter', 'latex');
+    title(sprintf('Quake Visco-Elastico | Re(u):azul, Im(u):rojo | FA: %.3f', ft), 'interpreter', 'latex');
     if disp_legend
         legend({'Re(u(z,t))', 'Im(u(z,t))'}, 'Location', 'southwest');
     end
